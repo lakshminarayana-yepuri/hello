8c8ad607e5da8e5d14e365f360ecd836c75f4cad
lakshminarayana-yepuri <79193132+lakshminarayana-yepuri@users.noreply.github.com>
2021-02-18 16:50:22 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = 'E-commerce project using python'
+copyright = '2020, lakshmi narayana yepuri'
+author = 'lakshmi narayana yepuri'
 
 # The full version, including alpha/beta/rc tags
 release = ''
